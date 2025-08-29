f463e33cda3d77fc7c6d28253a1e57098ac23da0
jkaylight <jkaylight@gmail.com>
2025-08-29 10:14:45 +0100

    fix: verce app.tsx import errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import FileUploader from './FileUploader'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="flex items-center justify-center h-screen bg-gray-50">
